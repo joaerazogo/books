1b87e1640b17048fbbde9fbdd692fa9b2b53eb62
joaerazogo <joaerazogo@unal.edu.co>
2018-05-22 07:10:06 -0500

    books changed

--- app/routes/book/books.js
+++ app/routes/book/books.js
@@ -2,22 +2,9 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model({id}){
-    // const book = this.modelFor('book');
-    // const author_book = book.findBy('id', id);
-    // const nameBook = author_book.get('title');
-    // const chapterBook = author_book.get('chapters');
-
-    // if(!author_book){
-    //   this.transitionTo('author');
-    // }else {
-    //   return{
-    //     id,
-    //     nameBook,
-    //     chapterBook,
-    //   };
-    // }
     const books = this.modelFor('book');
     const book = books.findBy('id', id);
+    //alert(books);
     if (!book) {
         this.transitionTo('author');
 
